a53a89196df386bc3f6393d1aae193dac6b42364
Ramil <ram_ram_14@list.ru>
2021-07-28 11:49:36 +0300

    исправил отступы #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,49 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'eslint-plugin', 'import'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin", "import"],
   env: {
     es6: true,
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'airbnb-base',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:eslint-plugin/recommended',
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:eslint-plugin/recommended",
   ],
   parserOptions: {
     ecmaVersion: 10,
-    sourceType: 'module',
+    sourceType: "module",
   },
   overrides: [
     // Testing rules
     {
-      files: ['src/**/*.ts'],
+      files: ["src/**/*.ts"],
       env: {
         mocha: true,
       },
       rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
+        "import/no-extraneous-dependencies": [
+          "error",
           { devDependencies: true },
         ],
       },
     },
   ],
   rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
       },
     ],
   },
